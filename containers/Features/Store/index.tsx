3210495d38ab450061d817891a28e6d9ec83a90c
wholespace <wholespace.214@gmail.com>
2022-03-10 08:11:58 +0900

    Complete features store page

--- containers/Features/Store/index.tsx
+++ containers/Features/Store/index.tsx
@@ -1,8 +1,367 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Styled from "./styled";
 
+import Image from "next/image";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+// import required modules
+import { Pagination } from "swiper";
+
+//TODO import components
+import Button from "@components/Button";
+
+//TODO import image
+import store1 from "@assets/images/store_1.png";
+import store2 from "@assets/images/store_2.png";
+import logo from "@assets/images/logo.png";
+import store1_mobile from "@assets/images/store_1_mobile.png";
+
+import store_about from "@assets/images/store_about.png";
+
+import store3 from "@assets/images/store_3.png";
+import store4 from "@assets/images/store_4.png";
+
+//TODO import icon
+import store_icon1 from "@assets/icons/store/1.png";
+import store_icon2 from "@assets/icons/store/2.png";
+import store_icon3 from "@assets/icons/store/3.png";
+import store_icon4 from "@assets/icons/store/4.png";
+import store_icon5 from "@assets/icons/store/5.png";
+import store_icon6 from "@assets/icons/store/6.png";
+import store_icon7 from "@assets/icons/store/7.png";
+import store_icon8 from "@assets/icons/store/8.png";
+
+import provide_icon1 from "@assets/icons/provide/provide_1.png";
+import provide_icon2 from "@assets/icons/provide/provide_2.png";
+import provide_icon3 from "@assets/icons/provide/provide_3.png";
+import provide_icon4 from "@assets/icons/provide/provide_4.png";
+
+//TODO  import partners icon
+import partners1 from "@assets/icons/partners/1.png";
+import partners2 from "@assets/icons/partners/2.png";
+import partners3 from "@assets/icons/partners/3.png";
+import partners4 from "@assets/icons/partners/4.png";
+import partners5 from "@assets/icons/partners/5.png";
+
+import OfferCard from "@components/OfferCard";
+
+import map from "@assets/icons/map.png";
+
+interface OfferDataType {
+  background: string;
+  icon: String;
+  title: String;
+  content: string;
+}
+const partners = [
+  partners1,
+  partners2,
+  "0",
+  "0",
+  partners3,
+  partners4,
+  partners5,
+];
+
 const Software: FC = () => {
-  return <Styled.SoftwareContainer>Store</Styled.SoftwareContainer>;
+  const [offerData, setOfferData] = useState<Array<OfferDataType>>([
+    {
+      background: "#D1D1D1",
+      icon: "hard",
+      title: "Shop online",
+      content:
+        "Shop online, reserve a firearm, setup an appointment to finalize purchase in-store",
+    },
+    {
+      background: "#D1D1D1",
+      icon: "hard",
+      title: "Support",
+      content:
+        "Instant access to our firearm specialists during the online experience to ask any questions about the firearms your interested in",
+    },
+    {
+      background: "#D1D1D1",
+      icon: "hard",
+      title: "Online selection",
+      content:
+        "No sales pressure, browser online and reserve at the customers convenience",
+    },
+    {
+      background: "#D1D1D1",
+      icon: "hard",
+      title: "Trust",
+      content:
+        "Maximum transparency and the elimination all of the guesswork on the customer’s side",
+    },
+    {
+      background: "#D1D1D1",
+      icon: "hard",
+      title: "Error checking",
+      content:
+        "Never worry about filling out and keeping physical records again, the majority of all the paperwork is done digitally (on-site). This allows real-time validations and will catch human error before submission.",
+    },
+  ]);
+
+  const [storeData, setStoreData] = useState<Array<StaticImageData>>([
+    store_icon1,
+    store_icon2,
+    store_icon3,
+    store_icon4,
+    store_icon5,
+    store_icon6,
+    store_icon7,
+    store_icon8,
+  ]);
+
+  return (
+    <Styled.SoftwareContainer>
+      <Styled.Header>
+        <Styled.HeaderText>
+          <Styled.HeaderTitle>Bay Area Firearms</Styled.HeaderTitle>
+          <Styled.HeaderContent>
+            It's 2022, yet the firearm industry still operates as if the
+            internet does not exist. We've modernized the process of purchasing
+            a firearm, removing the confusion and inevitable headache.
+          </Styled.HeaderContent>
+          <Styled.HeaderImageMobile>
+            <Image src={store1_mobile} />
+          </Styled.HeaderImageMobile>
+          <Styled.HeaderBtn>
+            <Button>view products</Button>
+          </Styled.HeaderBtn>
+        </Styled.HeaderText>
+        <Styled.HeaderImage>
+          <Styled.HeaderImage>
+            <Styled.HeaderImageOne>
+              <Image src={store1} />
+            </Styled.HeaderImageOne>
+            <Styled.HeaderImageTwo>
+              <Image src={store2} />
+            </Styled.HeaderImageTwo>
+            <Styled.HeaderImageLogo>
+              <Image src={logo} />
+            </Styled.HeaderImageLogo>
+          </Styled.HeaderImage>
+        </Styled.HeaderImage>
+      </Styled.Header>
+      <Styled.IconList>
+        {storeData.map((item, index) => (
+          <Styled.IconItem key={index}>
+            <Styled.IconItemImage>
+              <Image src={item} />
+            </Styled.IconItemImage>
+          </Styled.IconItem>
+        ))}
+      </Styled.IconList>
+      <Styled.About>
+        <Styled.AboutText>
+          <Styled.AboutTitle>About store</Styled.AboutTitle>
+          <Styled.AboutContent>
+            Browse our selection of firearms online and decide which is the best
+            fit for you with our questionnaire and live chat support services.
+            Avoid multiple trips to the firearm dealer and pushy salesman who
+            are determined on selling select firearms for their own gain.
+          </Styled.AboutContent>
+        </Styled.AboutText>
+        <Styled.AboutImage>
+          <Image src={store_about} />
+        </Styled.AboutImage>
+        <Styled.AboutAlter>
+          <Styled.AlterHeader>
+            <Styled.AlterIcon>
+              <Image src={map} />
+            </Styled.AlterIcon>
+            <Styled.AlterTitle>Locations</Styled.AlterTitle>
+          </Styled.AlterHeader>
+          <Styled.AlterText>
+            Headquartered in{" "}
+            <Styled.AlterTextRed>Redwood City, California,</Styled.AlterTextRed>{" "}
+            our services simplify firearm purchases while remaining compliant
+            with state laws.
+          </Styled.AlterText>
+        </Styled.AboutAlter>
+      </Styled.About>
+      <Styled.Benefits>
+        <Styled.BenefitsHeader>
+          <Styled.BenefitsTitle>service</Styled.BenefitsTitle>
+          <Styled.BenefitsText>
+            We provide a 2-year warranty for weapons purchased in our store. You
+            don't care about anything
+          </Styled.BenefitsText>
+        </Styled.BenefitsHeader>
+        <Styled.BenefitsSwiper>
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={115}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+          >
+            {offerData.map((item, index) => (
+              <SwiperSlide key={index}>
+                <OfferCard
+                  background={item.background}
+                  icon={item.icon}
+                  title={item.title}
+                  content={item.content}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </Styled.BenefitsSwiper>
+        <Styled.BenefitsSwiperMobile>
+          <Swiper
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Pagination]}
+          >
+            {offerData.map((item, index) => (
+              <SwiperSlide key={index}>
+                <OfferCard
+                  background={item.background}
+                  icon={item.icon}
+                  title={item.title}
+                  content={item.content}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </Styled.BenefitsSwiperMobile>
+      </Styled.Benefits>
+      <Styled.Manufacturing>
+        <Styled.ManufacturingContent>
+          <Styled.ManufacturingTitle>manufacturing</Styled.ManufacturingTitle>
+          <Styled.ManufacturingText>
+            Most retailers only sell other people's products, making them
+            dependent on distributors and manufacturers. With Dumpling Defense
+            manufacturing firearms, fewer middlemen are involved in the
+            product's sale. Our manufacturing opens doors to producing newer and
+            improved models of outdated guns that are no longer available for
+            purchase in California.
+          </Styled.ManufacturingText>
+        </Styled.ManufacturingContent>
+        <Styled.ManufacturingImage>
+          <Styled.ManufacturingImageOne>
+            <Image src={store4} />
+          </Styled.ManufacturingImageOne>
+          <Styled.ManufacturingImageTwo>
+            <Image src={store3} />
+          </Styled.ManufacturingImageTwo>
+        </Styled.ManufacturingImage>
+      </Styled.Manufacturing>
+
+      <Styled.Work>
+        <Styled.WorkTitle>How it work</Styled.WorkTitle>
+        <Styled.WorkTimeLine></Styled.WorkTimeLine>
+        <Styled.WorkList>
+          <Styled.WorkItem>
+            <Styled.WorkItemIcon>
+              <Image src={provide_icon1} />
+            </Styled.WorkItemIcon>
+            <Styled.WorkItemContent>
+              <Styled.WorkItemTitle>
+                Choose a Firearm Online
+              </Styled.WorkItemTitle>
+              <Styled.WorkItemText>
+                Browse through our firearm selection online and pick the best
+                option for you, your comfort, and your expertise.
+              </Styled.WorkItemText>
+            </Styled.WorkItemContent>
+          </Styled.WorkItem>
+          <Styled.WorkItem>
+            <Styled.WorkItemIcon>
+              <Image src={provide_icon2} />
+            </Styled.WorkItemIcon>
+            <Styled.WorkItemContent>
+              <Styled.WorkItemTitle>
+                Submit Documents Online
+              </Styled.WorkItemTitle>
+              <Styled.WorkItemText>
+                According to your state's laws, our software will directly send
+                the required documents to purchase and obtain a firearm legally.
+              </Styled.WorkItemText>
+            </Styled.WorkItemContent>
+          </Styled.WorkItem>
+          <Styled.WorkItem>
+            <Styled.WorkItemIcon>
+              <Image src={provide_icon3} />
+            </Styled.WorkItemIcon>
+            <Styled.WorkItemContent>
+              <Styled.WorkItemTitle>
+                Wait for Confirmation in the System
+              </Styled.WorkItemTitle>
+              <Styled.WorkItemText>
+                Patiently wait for our software to confirm and organize your
+                legal documentation in our system.
+              </Styled.WorkItemText>
+            </Styled.WorkItemContent>
+          </Styled.WorkItem>
+          <Styled.WorkItem>
+            <Styled.WorkItemIcon>
+              <Image src={provide_icon4} />
+            </Styled.WorkItemIcon>
+            <Styled.WorkItemContent>
+              <Styled.WorkItemTitle>
+                Pick Up Firearms in the Store
+              </Styled.WorkItemTitle>
+              <Styled.WorkItemText>
+                Once documentation is squared away, you will receive an alert
+                from Dumpling Defense inviting you to a one-time visit to pick
+                up your firearm.
+              </Styled.WorkItemText>
+            </Styled.WorkItemContent>
+          </Styled.WorkItem>
+        </Styled.WorkList>
+      </Styled.Work>
+      <Styled.Partners>
+        <Styled.PartnersText>Partners</Styled.PartnersText>
+        <Styled.PartnersList>
+          {/* <Styled.PartnersItem></Styled.PartnersItem> */}
+          {partners.map((item, index) => {
+            if (item === "0") {
+              return (
+                <Styled.PartnersItemNot key={index}></Styled.PartnersItemNot>
+              );
+            } else {
+              return (
+                <Styled.PartnersItem key={index}>
+                  <Image src={item} />
+                </Styled.PartnersItem>
+              );
+            }
+          })}
+        </Styled.PartnersList>
+        <Styled.PartnersListMobile>
+          <Styled.PartnersItem key={1}>
+            <Styled.PartnersItemImage>
+              <Image src={partners2} />
+            </Styled.PartnersItemImage>
+          </Styled.PartnersItem>
+          <Styled.PartnersItem key={2}>
+            <Styled.PartnersItemImage>
+              <Image src={partners2} />
+            </Styled.PartnersItemImage>
+          </Styled.PartnersItem>
+          <Styled.PartnersItem key={3}>
+            <Styled.PartnersItemImage>
+              <Image src={partners2} />
+            </Styled.PartnersItemImage>
+          </Styled.PartnersItem>
+          <Styled.PartnersItem key={4}>
+            <Styled.PartnersItemImage>
+              <Image src={partners2} />
+            </Styled.PartnersItemImage>
+          </Styled.PartnersItem>
+        </Styled.PartnersListMobile>
+      </Styled.Partners>
+    </Styled.SoftwareContainer>
+  );
 };
 
 export default Software;
--- containers/Features/Store/styled.ts
+++ containers/Features/Store/styled.ts
@@ -2,6 +2,581 @@ import styled from "styled-components";
 
 const SoftwareContainer = styled.div``;
 
+//! Header
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 50px 150px 200px 148px;
+  gap: 250px;
+  @media screen and (max-width: 768px) {
+    margin: 0 24px 9px 24px;
+    flex-direction: column;
+    gap: 16px;
+  }
+`;
+
+const HeaderText = styled.div`
+  flex: 1;
+  margin-top: 160px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const HeaderTitle = styled.div`
+  font-family: Heavitas;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 64px;
+  line-height: 70px;
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.01em;
+  color: #000000;
+
+  @media screen and (max-width: 768px) {
+    font-size: 40px;
+    line-height: 50px;
+  }
+`;
+
+const HeaderImageMobile = styled.div`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: flex;
+    margin-top: 32px;
+    margin-bottom: 24px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
+const HeaderContent = styled.div`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 24px;
+  display: flex;
+  align-items: center;
+
+  max-width: 475px;
+
+  margin-top: 16px;
+  @media screen and (max-width: 768px) {
+    max-width: 100%;
+  }
+`;
+
+const HeaderBtn = styled.div`
+  width: 229px;
+  margin-top: 32px;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    margin-top: 0;
+  }
+`;
+
+const HeaderImage = styled.div`
+  flex: 1;
+  position: relative;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const HeaderImageOne = styled.div``;
+
+const HeaderImageTwo = styled.div`
+  position: absolute;
+  top: 250px;
+  left: -120px;
+`;
+
+const HeaderImageLogo = styled.div`
+  position: absolute;
+  top: -70px;
+  right: -100px;
+`;
+
+const IconList = styled.div`
+  display: flex;
+  flex-direction: row;
+  background: #fbfbfb;
+  padding: 48px 0px 48px 0px;
+  @media screen and (max-width: 768px) {
+    overflow-x: scroll;
+    /* width: 1000px; */
+  }
+`;
+
+const IconItem = styled.div`
+  min-width: 170px;
+`;
+
+const IconItemImage = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+//! About
+
+const About = styled.div`
+  padding: 140px 146px 100px 261px;
+  position: relative;
+  @media screen and (max-width: 768px) {
+    padding: 0 23px 0 23px;
+  }
+`;
+
+const AboutText = styled.div``;
+
+const AboutTitle = styled.div`
+  font-family: "Heavitas [RUS by Daymarius]";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 40px;
+  line-height: 70px;
+  color: #252525;
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.01em;
+  @media screen and (max-width: 768px) {
+    line-height: 50px;
+  }
+`;
+
+const AboutContent = styled.div`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 40px;
+
+  max-width: 911px;
+  margin-top: 16px;
+  margin-bottom: 30px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 14px;
+    line-height: 24px;
+  }
+`;
+
+const AboutImage = styled.div`
+  max-width: 918px;
+  max-height: 532px;
+  margin-top: 50px;
+`;
+
+const AboutAlter = styled.div`
+  background: #f3f3f3;
+
+  display: flex;
+  flex-direction: column;
+
+  padding: 40px;
+
+  position: absolute;
+  top: 860px;
+  right: 125px;
+  @media screen and (max-width: 768px) {
+    position: unset;
+  }
+`;
+
+const AlterHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const AlterIcon = styled.div``;
+
+const AlterTitle = styled.div`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 21px;
+  line-height: 24px;
+  color: #000000;
+
+  margin-left: 8px;
+`;
+
+const AlterText = styled.div`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 24px;
+  color: #000000;
+
+  margin-left: 32px;
+  max-width: 462px;
+`;
+
+const AlterTextRed = styled.span`
+  color: red;
+`;
+
+const Benefits = styled.div`
+  background: #fbfbfb;
+  padding: 130px 31px 146px 147px;
+  @media screen and (max-width: 768px) {
+    padding: 82px 24px 150px 24px;
+  }
+`;
+
+const BenefitsHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    justify-content: none;
+  }
+`;
+
+const BenefitsTitle = styled.div`
+  font-family: "Heavitas [RUS by Daymarius]";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 40px;
+  line-height: 70px;
+
+  display: flex;
+  letter-spacing: 0.01em;
+
+  color: #272624;
+
+  @media screen and (max-width: 768px) {
+    line-height: 50px;
+  }
+`;
+
+const BenefitsText = styled.div`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 40px;
+
+  color: #000000;
+
+  max-width: 574px;
+  @media screen and (max-width: 768px) {
+    max-width: 100%;
+    font-size: 14px;
+    line-height: 24px;
+    margin-top: 16px;
+  }
+`;
+
+const BenefitsSwiper = styled.div`
+  margin-top: 56px;
+  cursor: pointer;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const BenefitsSwiperMobile = styled.div`
+  margin-top: 32px;
+  display: none;
+  cursor: pointer;
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
+`;
+
+//! Manufacturing
+
+const Manufacturing = styled.div`
+  margin-top: 96px;
+  @media screen and (max-width: 768px) {
+    padding: 0 24px 0 24px;
+  }
+`;
+
+const ManufacturingContent = styled.div`
+  padding: 0px 0px 0px 261px;
+  @media screen and (max-width: 768px) {
+    padding: 0 0 0 0;
+  }
+`;
+
+const ManufacturingText = styled.div`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 24px;
+  line-height: 40px;
+  max-width: 911px;
+  @media screen and (max-width: 768px) {
+    font-size: 14px;
+    line-height: 24px;
+  }
+`;
+
+const ManufacturingTitle = styled.div`
+  font-family: "Heavitas [RUS by Daymarius]";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 40px;
+  line-height: 70px;
+
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.01em;
+
+  @media screen and (max-width: 768px) {
+    line-height: 50px;
+    font-size: 32px;
+    /* max-width: 287px; */
+  }
+`;
+
+const ManufacturingImage = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 6px;
+  margin-left: 42px;
+  @media screen and (max-width: 768px) {
+    /* display: none; */
+    margin: 0;
+  }
+`;
+const ManufacturingImageOne = styled.div`
+  margin-top: 181px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+const ManufacturingImageTwo = styled.div`
+  margin-left: 111px;
+  @media screen and (max-width: 768px) {
+    /* display: none; */
+    display: flex;
+    margin: 32px auto 0 auto;
+  }
+`;
+
+//! Work
+
+const Work = styled.div`
+  padding: 74px 0px 0px 340px;
+  position: relative;
+  @media screen and (max-width: 768px) {
+    padding: 154px 24px 0 24px;
+  }
+`;
+
+const WorkTitle = styled.div`
+  font-family: "Heavitas [RUS by Daymarius]";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 40px;
+  line-height: 70px;
+
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.01em;
+`;
+
+const WorkList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 80px;
+  margin-top: 55px;
+  @media screen and (max-width: 768px) {
+    gap: 40px;
+  }
+`;
+
+const WorkItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 32px;
+`;
+
+const WorkItemTitle = styled.div`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 21px;
+  line-height: 24px;
+`;
+
+const WorkItemContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const WorkItemText = styled.div`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 24px;
+
+  margin-top: 16px;
+
+  max-width: 568px;
+`;
+
+const WorkItemIcon = styled.div`
+  display: flex;
+  align-items: baseline;
+`;
+
+const WorkTimeLine = styled.div`
+  border: 1px solid #e9342e;
+  height: 500px;
+  width: 0px;
+
+  position: absolute;
+  top: 200px;
+  left: 372px;
+  @media screen and (max-width: 768px) {
+    top: 300px;
+    left: 38px;
+  }
+`;
+
+//! Partners
+
+const Partners = styled.div`
+  padding: 226px 148px 0px 261px;
+  @media screen and (max-width: 768px) {
+    padding: 0 0 0 26px;
+    margin-top: 110px;
+  }
+`;
+
+const PartnersText = styled.div`
+  font-family: Heavitas;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 40px;
+  line-height: 50px;
+  display: flex;
+  align-items: center;
+  letter-spacing: 0.01em;
+  color: #272624;
+`;
+
+const PartnersList = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+
+  margin-top: 58px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const PartnersListMobile = styled.div`
+  display: none;
+  margin-top: 40px;
+  margin-bottom: 96px;
+  @media screen and (max-width: 768px) {
+    display: flex;
+    flex-wrap: wrap;
+  }
+`;
+
+const PartnersItem = styled.div`
+  width: 330px;
+  height: 330px;
+  border: 1px solid #d1d1d1;
+  box-sizing: border-box;
+  display: flex;
+  align-content: center;
+  align-items: center;
+  justify-content: center;
+  @media screen and (max-width: 768px) {
+    max-width: 164px;
+    max-height: 164px;
+  }
+`;
+
+const PartnersItemImage = styled.div`
+  max-width: 100px;
+  max-height: 100px;
+`;
+
+const PartnersItemNot = styled.div`
+  width: 330px;
+  height: 330px;
+`;
+
 export default {
   SoftwareContainer,
+
+  //! Header
+  Header,
+  HeaderText,
+  HeaderTitle,
+  HeaderContent,
+  HeaderBtn,
+  HeaderImage,
+  HeaderImageOne,
+  HeaderImageTwo,
+  HeaderImageLogo,
+  HeaderImageMobile,
+
+  //! Icon
+  IconList,
+  IconItem,
+  IconItemImage,
+
+  //! About
+  About,
+  AboutText,
+  AboutContent,
+  AboutTitle,
+  AboutImage,
+  AboutAlter,
+  AlterHeader,
+  AlterIcon,
+  AlterTitle,
+  AlterText,
+  AlterTextRed,
+
+  //! Benefits
+  Benefits,
+  BenefitsHeader,
+  BenefitsTitle,
+  BenefitsText,
+  BenefitsSwiper,
+  BenefitsSwiperMobile,
+
+  //! Manufacturing
+  Manufacturing,
+  ManufacturingContent,
+  ManufacturingText,
+  ManufacturingImage,
+  ManufacturingTitle,
+  ManufacturingImageOne,
+  ManufacturingImageTwo,
+
+  //! Work
+  Work,
+  WorkTitle,
+  WorkTimeLine,
+  WorkList,
+  WorkItem,
+  WorkItemContent,
+  WorkItemIcon,
+  WorkItemText,
+  WorkItemTitle,
+
+  //! Partners
+  Partners,
+  PartnersText,
+  PartnersList,
+  PartnersItem,
+  PartnersItemNot,
+  PartnersListMobile,
+  PartnersItemImage,
 };
